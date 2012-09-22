c35bd413ca5610a733a294e9aa7d25eb4229a3b4
KDegrave_cp <SND\KDegrave_cp>
2012-09-22 09:41:29 +0000

    changing build.bat

--- Linq2IndexedDB/Linq2IndexedDB/Packages/build.bat
+++ Linq2IndexedDB/Linq2IndexedDB/Packages/build.bat
@@ -1,5 +1,5 @@
-﻿IF "%NuGet%"=="" SET NuGet=nuget.exe
-IF "%SourcesPath%"=="" SET SourcesPath=.
+﻿if "%NuGet%"=="" SET NuGet=nuget.exe 
+if "%SourcesPath%"=="" SET SourcesPath=. 
 
 %NuGet% pack %SourcesPath%\Linq2IndexedDB\Packages\IndexedbDBViewer.nuspec -version 1.0.4
 %NuGet% pack %SourcesPath%\Linq2IndexedDB\Packages\Linq2IndexedDB.nuspec -version 1.0.16
