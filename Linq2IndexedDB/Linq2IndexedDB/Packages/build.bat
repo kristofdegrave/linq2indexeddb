cbdf56cd61f89f3a9fbe87ccce79b9f37967f964
KDegrave_cp <SND\KDegrave_cp>
2012-10-28 08:57:38 +0000

    * Fixing issue with the nuget packages

--- Linq2IndexedDB/Linq2IndexedDB/Packages/build.bat
+++ Linq2IndexedDB/Linq2IndexedDB/Packages/build.bat
@@ -1,6 +1,6 @@
 if "%NuGet%"=="" SET NuGet=nuget.exe 
 if "%SourcesPath%"=="" SET SourcesPath=. 
 
-%NuGet% pack %SourcesPath%\Linq2IndexedDB\Linq2IndexedDB\Packages\IndexedbDBViewer.nuspec -version 1.0.7
-%NuGet% pack %SourcesPath%\Linq2IndexedDB\Linq2IndexedDB\Packages\Linq2IndexedDB.nuspec -version 1.0.19
-%NuGet% pack %SourcesPath%\Linq2IndexedDB\Linq2IndexedDB\Packages\Linq2IndexedDBWin8.nuspec -version 1.0.19
+%NuGet% pack %SourcesPath%\Linq2IndexedDB\Linq2IndexedDB\Packages\IndexedbDBViewer.nuspec -version 1.0.8
+%NuGet% pack %SourcesPath%\Linq2IndexedDB\Linq2IndexedDB\Packages\Linq2IndexedDB.nuspec -version 1.0.20
+%NuGet% pack %SourcesPath%\Linq2IndexedDB\Linq2IndexedDB\Packages\Linq2IndexedDBWin8.nuspec -version 1.0.20
