d42ffce361c63289850cb3601d6ce26c5309b906
KDegrave_cp <SND\KDegrave_cp>
2014-03-15 10:33:23 +0000

    Adjustments for myget build

--- Linq2IndexedDB/Linq2IndexedDB/Packages/build.bat
+++ Linq2IndexedDB/Linq2IndexedDB/Packages/build.bat
@@ -1,5 +1,3 @@
-if "%NuGet%"=="" SET NuGet=nuget.exe 
-
-%NuGet% pack %SourcesPath%\Packages\IndexedbDBViewer.nuspec -version 1.2.0
-%NuGet% pack %SourcesPath%\Packages\Linq2IndexedDB.nuspec -version 1.1.0
-%NuGet% pack %SourcesPath%\Packages\Linq2IndexedDBWin8.nuspec -version 1.1.0
+%NuGet% pack %SourcesPath%Packages\IndexedbDBViewer.nuspec -version 1.2.0
+%NuGet% pack %SourcesPath%Packages\Linq2IndexedDB.nuspec -version 1.1.0
+%NuGet% pack %SourcesPath%Packages\Linq2IndexedDBWin8.nuspec -version 1.1.0
