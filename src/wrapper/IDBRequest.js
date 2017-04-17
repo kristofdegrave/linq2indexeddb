7b7dacd9febefd925d2eba13408abd364e188f5b
Kristof Degrave <kristof.degrave@telenet.be>
2017-04-17 21:24:44 +0200

    Added IDBRequest wrapper

---
+++ src/wrapper/IDBRequest.js
@@ -0,0 +1,52 @@
+/**
+ * https://w3c.github.io/IndexedDB/#idbrequest
+ */
+
+class IDBRequest {
+    constructor(request) {
+        this._request = request;
+        this.promise = new Promise((resolve, reject) => {
+            request.onsuccess = event => {
+                resolve(event);
+                if (this.onsuccess){
+                    this.onsuccess(event);
+                }
+            };
+            request.onerror = event => {
+                reject(event);
+            };
+        });
+    }
+    get result() {
+        return this.originalRequest.result;
+    }
+    get error() {
+        return this.originalRequest.error;
+    }
+    get source() {
+        return this.originalRequest.source;
+    }
+    get transation() {
+        return this.originalRequest.transation;
+    }
+    get readyState() {
+        return this.originalRequest.readyState;
+    }
+    get onsuccess(){
+        return this._onsuccess;
+    }
+    set onsuccess(value){
+        this._onsuccess = value;
+    }
+    get onerror(){
+        return this._onerror;
+    }
+    set onerror(value){
+        this._onerror = value;
+    }
+    get originalRequest(){
+        return this._request;
+    }
+}
+
+export default IDBRequest;
